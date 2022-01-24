be187c9b73a0f6819319aa114249722e82ec512b
ganzai-san <ganzai@protonmail.com>
2022-01-23 21:53:14 -0500

    add transport layers to the router

--- cmd/daemon.go
+++ cmd/daemon.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/handler/extension"
+	"github.com/99designs/gqlgen/graphql/handler/lru"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
 	graph "github.com/glifio/graph/gql"
@@ -31,12 +32,12 @@ var daemonCmd = &cobra.Command{
   Short: "Start the graphQL server",
   Long:  `Start the graphQL server`,
   Run: func(cmd *cobra.Command, args []string) {
-	config, err := util.LoadConfig(".")
+	config, _ := util.LoadConfig(".")
 
     fmt.Println("start the graphQL server")
 	// Create a new connection to our pg database
 	var db postgres.Database
-	err = db.New(config.DbHost, config.DbPort, config.DbUser, config.DbPassword, config.DbDatabase, config.DbSchema)
+	err := db.New(config.DbHost, config.DbPort, config.DbUser, config.DbPassword, config.DbDatabase, config.DbSchema)
 
 	if err != nil {
 		log.Fatal(err)
@@ -74,7 +75,6 @@ var daemonCmd = &cobra.Command{
 			BlockService: blockService, 
 		},
 	}))
-	srv.AddTransport(transport.POST{})
 	srv.AddTransport(transport.Websocket{
 		KeepAlivePingInterval: 10 * time.Second,
 		Upgrader: websocket.Upgrader{
@@ -84,7 +84,17 @@ var daemonCmd = &cobra.Command{
 			},
 		},
 	})
+	srv.AddTransport(transport.Options{})
+	srv.AddTransport(transport.GET{})
+	srv.AddTransport(transport.POST{})
+	srv.AddTransport(transport.MultipartForm{})
+
+	srv.SetQueryCache(lru.New(1000))
+
 	srv.Use(extension.Introspection{})
+	srv.Use(extension.AutomaticPersistedQuery{
+		Cache: lru.New(100),
+	})
 
 	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	router.Handle("/query", srv)
